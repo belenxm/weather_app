e873b2a14777f622e474c6bc22888a955d1d66e8
fer.belen <belenxmorales@gmail.com>
2023-07-12 15:38:29 -0300

    props declarados y funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ function App() {
     const [lat, lon] = searchData.value.split("");
 
     const currentWeatherfetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}&units=metric`
     );
     const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}&units=metric`
     );
 
     Promise.all([currentWeatherfetch, forecastFetch])
@@ -39,7 +39,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -1,35 +1,41 @@
 import "./current-weather.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Belgrade</p>
-          <p className="weather-description">Sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img src="icons/01d.png" className="weather-icon" alt="weather" />
+        <img
+          src={`icons/${data.weather[0].icon}.png`}
+          className="weather-icon"
+          alt="weather"
+        />
         <div className="bottom">
-          <p className="temperature">18°C</p>
+          <p className="temperature">{Math.round(data.main.temp)}°C</p>
           <div className="details">
             <div className="parameter-row">
               <span className="parameter-label">Details</span>
             </div>
             <div className="parameter-row">
-              <span className="parameter-label">Feels like</span>
+              <span className="parameter-label">
+                {Math.round(data.main.feels_like)}°C
+              </span>
               <span className="parameter-value"> 22°C</span>
             </div>
             <div className="parameter-row">
               <span className="parameter-label">Wind</span>
-              <span className="parameter-value">2 m/s </span>
+              <span className="parameter-value">{data.wind.speed} m/s</span>
             </div>
             <div className="parameter-row">
               <span className="parameter-label">Humidity</span>
-              <span className="parameter-value">15 m/s </span>
+              <span className="parameter-value">{data.main.humidity} %</span>
             </div>
             <div className="parameter-row">
               <span className="parameter-label">Pressure</span>
-              <span className="parameter-value">15 hpa </span>
+              <span className="parameter-value">{data.main.pressure} hpa</span>
             </div>
           </div>
         </div>
